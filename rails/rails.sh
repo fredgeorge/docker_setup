216fad8cbc4246bfa454adac0f11296785565d77
Fred George <fredgeorge@acm.org>
2016-08-25 07:27:42 -0700

    Fred: Added a viable Rails 5 image/container setup

--- rails/rails.sh
+++ rails/rails.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --name='rails_5' -it -v $HOME/src/ruby:/ruby -w /ruby fredgeorge/rails:latest bash
+docker run --name='rails_5' -it -v $HOME/src/ruby:/ruby -w /ruby -p 3000:3000 fredgeorge/rails:latest bash

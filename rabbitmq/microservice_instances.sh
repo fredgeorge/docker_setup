1f1882d29a3e38851e0bb0fed08a595456cab7fd
Fred George <fredgeorge@acm.org>
2017-02-07 12:48:18 -0800

    Fred: Added more instances of RabbitMQ

--- rabbitmq/microservice_instances.sh
+++ rabbitmq/microservice_instances.sh
@@ -11,6 +11,9 @@ docker run -d -e RABBITMQ_NODENAME=microservice_rabbitmq -p 15679:15672 -p 5679:
 docker run -d -e RABBITMQ_NODENAME=microservice_rabbitmq -p 15680:15672 -p 5680:5672 --name rabbitmq10 rabbitmq:management
 docker run -d -e RABBITMQ_NODENAME=microservice_rabbitmq -p 15681:15672 -p 5681:5672 --name rabbitmq11 rabbitmq:management
 docker run -d -e RABBITMQ_NODENAME=microservice_rabbitmq -p 15682:15672 -p 5682:5672 --name rabbitmq12 rabbitmq:management
+docker run -d -e RABBITMQ_NODENAME=microservice_rabbitmq -p 15683:15672 -p 5683:5672 --name rabbitmq13 rabbitmq:management
+docker run -d -e RABBITMQ_NODENAME=microservice_rabbitmq -p 15684:15672 -p 5684:5672 --name rabbitmq14 rabbitmq:management
+docker run -d -e RABBITMQ_NODENAME=microservice_rabbitmq -p 15685:15672 -p 5685:5672 --name rabbitmq15 rabbitmq:management
 
 # If using VirtualBox, you will need to open up the host ports 156xx and 56xx
 #   VirtalBox > default (machine) > Settings > Network > Advanced > Port Forwarding
@@ -22,3 +25,14 @@ docker run -d -e RABBITMQ_NODENAME=microservice_rabbitmq -p 15682:15672 -p 5682:
 #     Guest IP: <blank>
 #     Guest Port: 56xx or 156xx
 
+# If using Windows 10 Docker, you will need to open up the host ports 156xx and 56xx
+#     You can manually permit a program to access the internet by opening a firewall port. You will need to know what port it uses and the protocol to make this work.
+#         1.	Navigate to Control Panel, System and Security and Windows Firewall.
+#         2.	Select Advanced settings and highlight Inbound Rules in the left pane.
+#         3.	Right click Inbound Rules and select New Rule.
+#         4.	Add the port you need to open and click Next.
+#         5.	Add the protocol (TCP or UDP) and the port number into the next window and click Next.
+#         6.	Select Allow the connection in the next window and hit Next.
+#         7.	Select the network type as you see fit and click Next.
+#         8.	Name the rule something meaningful and click Finish.
+#         You have now opened a firewall port in Windows 10!
--- rabbitmq/rm_instances.sh
+++ rabbitmq/rm_instances.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Remove all MicroService RabbitMQ containers
-docker rm rabbitmq03 rabbitmq04 rabbitmq05 rabbitmq06 rabbitmq07 rabbitmq08 rabbitmq09 rabbitmq10 rabbitmq11 rabbitmq12
+docker rm rabbitmq03 rabbitmq04 rabbitmq05 rabbitmq06 rabbitmq07 rabbitmq08 rabbitmq09 rabbitmq10 rabbitmq11 rabbitmq12 rabbitmq13 rabbitmq14 rabbitmq15

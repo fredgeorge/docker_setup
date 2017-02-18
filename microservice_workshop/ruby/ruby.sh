8dda3ee8978ef2ff04ac3b11ad155313dfc7f858
Fred George <fredgeorge@acm.org>
2017-02-18 06:03:57 +0100

    Fred: Corrected technique for starting multiple Ruby services in a single container

--- microservice_workshop/ruby/ruby.sh
+++ microservice_workshop/ruby/ruby.sh
@@ -1,16 +1,17 @@
 #!/bin/bas
 
 # In this alternative strategy, one Ruby instance can run multiple services.
-# First, start a Ruby instance as a daemon:
+# First, start a Ruby instance with the Monitor running:
 
 # Monitor: Docker run command (for Mac):
-docker run --name='workshop_ruby' -d -v /Users/dev/src/microservice_workshop/ruby:/workshop -w /workshop/rental_offer fredgeorge/microservice_ruby:latest bash
-
+docker run --name='workshop_ruby' -it -v /Users/dev/src/microservice_workshop/ruby:/workshop -w /workshop/rental_offer fredgeorge/microservice_ruby:latest ruby rental_offer_monitor.rb 10.13.154.34 5672
 # Monitor: Docker run command (for Windows 10):
-# docker run --name='workshop_ruby' -d -v c:/Users/dev/src/microservice_workshop/ruby:/workshop -w /workshop/rental_offer fredgeorge/microservice_ruby:latest bash
-
-# Next, start up Monitor by creating a new bash instance:
-docker exec -i -t workshop_ruby ruby rental_offer_monitor.rb 10.13.154.34 5672
+# docker run --name='workshop_ruby' -it -v c:/Users/dev/src/microservice_workshop/ruby:/workshop -w /workshop/rental_offer fredgeorge/microservice_ruby:latest ruby rental_offer_monitor.rb 10.13.154.34 5672
 
 # Then in another cmd/power_shell windoe, start up Need
-docker exec -i -t workshop_ruby ruby rental_offer_need.rb 10.13.154.34 5672
+docker exec -it workshop_ruby ruby rental_offer_need.rb 10.13.154.34 5672
+
+# Stopping the Monitor will stop both services, and exit the container
+
+# Restart the Ruby Monitor container to resume processing:
+# docker start -it workshop_ruby

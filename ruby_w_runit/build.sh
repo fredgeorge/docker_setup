bd308941e001040da7d0d74e3376605688219f31
Fred George <fredgeorge@acm.org>
2018-03-04 16:53:18 +0530

    Fred: Updated versions of various images

--- ruby_w_runit/build.sh
+++ ruby_w_runit/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker build --tag="fredgeorge/ruby_w_runit:latest" .
-docker tag fredgeorge/ruby_w_runit:latest fredgeorge/ruby_w_runit:v1.03
+docker tag fredgeorge/ruby_w_runit:latest fredgeorge/ruby_w_runit:v1.04

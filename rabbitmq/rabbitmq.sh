6245f6e0cbd62358c392c68be89f4212bbbc2d53
Fred George <fredgeorge@acm.org>
2019-08-30 09:19:58 +1200

    Fred: Updates to image names

--- rabbitmq/rabbitmq.sh
+++ rabbitmq/rabbitmq.sh
@@ -1,2 +1,4 @@
 #!/bin/bash 
-docker run -d -e RABBITMQ_NODENAME=microservice_rabbitmq -p 15672:15672 -p 5672:5672 --name rabbitmq rabbitmq:management
+docker run -d -e RABBITMQ_NODENAME=microservice_rabbitmq -p 15672:15672 -p 5672:5672 --name rabbitmq rabbitmq:3.7.17-management
+# Alternative RabbitMQ with vanilla ports
+# docker run -d -e RABBITMQ_NODENAME=microservice_rabbitmq -p 8080:15672 -p 80:5672 --name rabbitmq80 rabbitmq:3.7.17-management

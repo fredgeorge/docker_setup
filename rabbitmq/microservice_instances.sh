0c9d7f87b1628342ba694934908d81d8ff766bc1
Fred George <fredgeorge@acm.org>
2019-10-15 16:19:40 +0200

    Fred: Updated the version of RabbitMQ being used

--- rabbitmq/microservice_instances.sh
+++ rabbitmq/microservice_instances.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # Create instances with different ports
-docker run -d -e RABBITMQ_NODENAME=microservice_rabbitmq -p 15673:15672 -p 5673:5672 --name rabbitmq03 rabbitmq:3.7.17-management
-docker run -d -e RABBITMQ_NODENAME=microservice_rabbitmq -p 15674:15672 -p 5674:5672 --name rabbitmq04 rabbitmq:3.7.17-management
-docker run -d -e RABBITMQ_NODENAME=microservice_rabbitmq -p 15675:15672 -p 5675:5672 --name rabbitmq05 rabbitmq:3.7.17-management
-docker run -d -e RABBITMQ_NODENAME=microservice_rabbitmq -p 15676:15672 -p 5676:5672 --name rabbitmq06 rabbitmq:3.7.17-management
-docker run -d -e RABBITMQ_NODENAME=microservice_rabbitmq -p 15677:15672 -p 5677:5672 --name rabbitmq07 rabbitmq:3.7.17-management
-docker run -d -e RABBITMQ_NODENAME=microservice_rabbitmq -p 15678:15672 -p 5678:5672 --name rabbitmq08 rabbitmq:3.7.17-management
-docker run -d -e RABBITMQ_NODENAME=microservice_rabbitmq -p 15679:15672 -p 5679:5672 --name rabbitmq09 rabbitmq:3.7.17-management
-docker run -d -e RABBITMQ_NODENAME=microservice_rabbitmq -p 15680:15672 -p 5680:5672 --name rabbitmq10 rabbitmq:3.7.17-management
-docker run -d -e RABBITMQ_NODENAME=microservice_rabbitmq -p 15681:15672 -p 5681:5672 --name rabbitmq11 rabbitmq:3.7.17-management
-docker run -d -e RABBITMQ_NODENAME=microservice_rabbitmq -p 15682:15672 -p 5682:5672 --name rabbitmq12 rabbitmq:3.7.17-management
-docker run -d -e RABBITMQ_NODENAME=microservice_rabbitmq -p 15683:15672 -p 5683:5672 --name rabbitmq13 rabbitmq:3.7.17-management
-docker run -d -e RABBITMQ_NODENAME=microservice_rabbitmq -p 15684:15672 -p 5684:5672 --name rabbitmq14 rabbitmq:3.7.17-management
-docker run -d -e RABBITMQ_NODENAME=microservice_rabbitmq -p 15685:15672 -p 5685:5672 --name rabbitmq15 rabbitmq:3.7.17-management
+docker run -d -e RABBITMQ_NODENAME=microservice_rabbitmq -p 15673:15672 -p 5673:5672 --name rabbitmq03 rabbitmq:3.7-management
+docker run -d -e RABBITMQ_NODENAME=microservice_rabbitmq -p 15674:15672 -p 5674:5672 --name rabbitmq04 rabbitmq:3.7-management
+docker run -d -e RABBITMQ_NODENAME=microservice_rabbitmq -p 15675:15672 -p 5675:5672 --name rabbitmq05 rabbitmq:3.7-management
+docker run -d -e RABBITMQ_NODENAME=microservice_rabbitmq -p 15676:15672 -p 5676:5672 --name rabbitmq06 rabbitmq:3.7-management
+docker run -d -e RABBITMQ_NODENAME=microservice_rabbitmq -p 15677:15672 -p 5677:5672 --name rabbitmq07 rabbitmq:3.7-management
+docker run -d -e RABBITMQ_NODENAME=microservice_rabbitmq -p 15678:15672 -p 5678:5672 --name rabbitmq08 rabbitmq:3.7-management
+docker run -d -e RABBITMQ_NODENAME=microservice_rabbitmq -p 15679:15672 -p 5679:5672 --name rabbitmq09 rabbitmq:3.7-management
+docker run -d -e RABBITMQ_NODENAME=microservice_rabbitmq -p 15680:15672 -p 5680:5672 --name rabbitmq10 rabbitmq:3.7-management
+docker run -d -e RABBITMQ_NODENAME=microservice_rabbitmq -p 15681:15672 -p 5681:5672 --name rabbitmq11 rabbitmq:3.7-management
+docker run -d -e RABBITMQ_NODENAME=microservice_rabbitmq -p 15682:15672 -p 5682:5672 --name rabbitmq12 rabbitmq:3.7-management
+docker run -d -e RABBITMQ_NODENAME=microservice_rabbitmq -p 15683:15672 -p 5683:5672 --name rabbitmq13 rabbitmq:3.7-management
+docker run -d -e RABBITMQ_NODENAME=microservice_rabbitmq -p 15684:15672 -p 5684:5672 --name rabbitmq14 rabbitmq:3.7-management
+docker run -d -e RABBITMQ_NODENAME=microservice_rabbitmq -p 15685:15672 -p 5685:5672 --name rabbitmq15 rabbitmq:3.7-management
 
 # If using VirtualBox, you will need to open up the host ports 156xx and 56xx
 #   VirtalBox > default (machine) > Settings > Network > Advanced > Port Forwarding
